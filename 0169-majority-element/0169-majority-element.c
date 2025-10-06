0e2ed3f0c12551fb2cdf5f10249ed89ed97c6675
Santhoshi Anaparthi <147791940+Santhoshi003@users.noreply.github.com>
2025-10-06 22:31:29 +0530

    Time: 0 ms (100%), Space: 10.3 MB (39.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,19 @@
+int majorityElement(int* nums, int numsSize) {
+    int major=nums[0];
+    int occ=1;
+    for(int i=1;i<numsSize;i++)
+    {
+        if(major!=nums[i]){
+               occ--;
+        }
+        else{
+            occ++;
+        }
+        if(occ==0)
+        {
+            major=nums[i];
+            occ=1;
+        }
+    }
+    return major;
+}
